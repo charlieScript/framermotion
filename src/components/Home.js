0a42f4e9578c36c66bdbc33fca7764327d18bbc9
charles <gozione3@gmail.com>
2020-08-07 12:46:12 +0100

    lesson 3 - initial prop

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,18 @@ import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <motion.div className="home container" animate={{
-      opacity: 0.5, marginTop: '1.5rem'
+    <motion.div className="home container" 
+    initial={{
+      opacity: 0
+    }} animate={{
+      // opacity: 0.5, marginTop: '1.5rem'
+      opacity: 1
     }}>
       <motion.h2
         animate={{
-          fontSize: 50,
-          color: "red",
-          x: 100,
+          // fontSize: 50,
+          // color: "red",
+          // x: 100,
         }}
       >
         Welcome to Pizza Joint
@@ -19,7 +23,7 @@ const Home = () => {
       <Link to="/base">
         <motion.button
           animate={{
-            scale: 1.5,
+            // scale: 1.5,
           }}
         >
           Create Your Pizza
