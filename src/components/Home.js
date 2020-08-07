4e63b7792bdcc63f10d195ac5b02e0aca9820db3
charles <gozione3@gmail.com>
2020-08-07 13:01:02 +0100

    lesson - ?? using type sprinf,tween

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,28 +4,33 @@ import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <motion.div className="home container" 
-    initial={{
-      opacity: 0
-    }} animate={{
-      // opacity: 0.5, marginTop: '1.5rem'
-      opacity: 1
-    }}>
+    <motion.div
+      className="home container"
+      transition={{ delay: 1.5, duration: 1 }}
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        // opacity: 0.5, marginTop: '1.5rem'
+        opacity: 1,
+      }}>
       <motion.h2
-        animate={{
-          // fontSize: 50,
-          // color: "red",
-          // x: 100,
-        }}
-      >
+        animate={
+          {
+            // fontSize: 50,
+            // color: "red",
+            // x: 100,
+          }
+        }>
         Welcome to Pizza Joint
       </motion.h2>
       <Link to="/base">
         <motion.button
-          animate={{
-            // scale: 1.5,
-          }}
-        >
+          animate={
+            {
+              // scale: 1.5,
+            }
+          }>
           Create Your Pizza
         </motion.button>
       </Link>
